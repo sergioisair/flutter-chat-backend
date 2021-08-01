4ac94e03bff278a3cca1a41406c96a36c5e1c3b9
Sergio Isair <sergioisair@gmail.com>
2021-08-01 01:38:20 -0500

    Fin del backend autenticación en node Js

---
+++ helpers/jwt.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = ( uid ) => {
+
+    return new Promise((resolve, reject) => {
+        const payload = {
+            uid
+        };
+    
+        jwt.sign(payload, process.env.JWT_KEY, {
+            expiresIn: '24h'
+        }, (err, token) => {
+            if( err ){
+                // no se pudo crear el token
+                reject('No se pudo generar el JWT');
+            } else{
+                // si se pudo
+                resolve(token);
+            }
+        } );
+    });
+
+}
+
+module.exports={
+    generarJWT
+}
